66e422c279a63392dc1c2a7f2d1a5ad670072c4c
soydeif <daed_diaz@hotmail.com>
2021-08-04 17:29:24 +0000

    maquetado inicial

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4">
+			{/*<p>
+				<img src={rigoImageUrl} />
+			</p>
+			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
+			<p>
+				This boilerplate comes with lots of documentation:{" "}
+				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+					Read documentation
+				</a>
+			</p>*/}
+			<ul className="nav">
+				<li className="nav-item">
+					<a className="nav-link active" href="#">
+						Inicio
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Nosotros
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Destinos y Hoteles
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Empresas
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Contacto
+					</a>
+				</li>
+			</ul>
+
+			<div className="d-flex flex-column my-2 my-lg-0 flex-md-row align-items-center p-3 px-md-4 mb-3  ">
+				<nav className="my-2 my-md-0 mr-md-3 btn btn-outline-success my-2 my-sm-0">
+					<a className="p-2 text-dark" href="#">
+						Registrarse
+					</a>
+				</nav>
+				<a className="btn btn-outline-primary" href="#">
+					Iniciar sesión
+				</a>
 			</div>
-		</nav>
+		</div>
 	);
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,22 +3,19 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
+import { Nosotros } from "../component/nosotros";
+import { Destinos } from "../component/destinos";
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div>
+			<h1>TRAVIA</h1>
+
+			<p>Etiam efficitur dictum magna. Maecenas ornare justo at lectus dictum, eget lacinia arcu finibus.</p>
+			<Nosotros />
+			<Destinos />
 		</div>
 	);
 };
