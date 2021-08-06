557c3ce4b5e0759381e51ed2729dc6acfce043a4
psoto-go <pabletesglez@gmail.com>
2021-08-06 19:07:31 +0000

    mati te quiero

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,45 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Hotel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
+    location = db.Column(db.String(100), unique=False, nullable=False)
+    rooms = db.relationship('Room', backref='hotel', lazy=True)
+
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "description": self.description,
+            "location": self.location,
+            "rooms": list(map(lambda x:x.serialize(), self.rooms)),
+
             # do not serialize the password, its a security breach
         }
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(120), unique=False, nullable=False)
+    number_of_beds = db.Column(db.Integer, unique=False, nullable=False)
+    availability = db.Column(db.Date, unique=False, nullable=False)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'),
+        nullable=False)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "kind": self.kind,
+            "number_of_beds": self.number_of_beds,
+            "availability": self.availability
+            # do not serialize the password, its a security breach
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(120), unique=False, nullable=False)
+    number_of_beds = db.Column(db.Integer, unique=False, nullable=False)
+    availability = db.Column(db.Date, unique=False, nullable=False)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id')
