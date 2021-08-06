0a6caf06bc29266658b64525bbc2bd05fd0a4ffe
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-08-06 12:08:25 +0000

    r

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,27 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            
+        }
+
+    def check_password(self, password_param):
+        return safe_str_cmp(self.password.encode('utf-8'), password_param.encode('utf-8'))
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    tipo = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Admin %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "tipo": self.tipo
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Admin
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,66 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/sign_up', methods=['POST']) 
+def sign_up_user():
+
+    body_params = request.get_json()
+    print(body_params)
+    name = body_params.get("name", None)
+    last_name = body_params.get("last_name", None)
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+
+    
+    user1 = User(name=name, last_name = last_name, email=email, password=password)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "El usuario fue creado exitosamente"}), 200
+
+
+@api.route('/sign_in', methods=['POST']) 
+def sign_in_user_b():
+
+    body_params = request.get_json()
+
+    email = body_params.get("email", None)
+    password= body_params.get("password", None)
+
+    if email == None or password == None:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+
+    user= User.query.filter_by(email=email).one_or_none()
+    if not user or not user.check_password(password):
+        return jsonify("Tus credenciales son incorrectas, por favor intentelo nuevamente"), 401
+    
+
+    
+    acces_token = create_access_token(identity=email)
+
+    return jsonify({"acces_token": acces_token})
+
+   #User
+ # --------------------------------------------------------------------------------------------
+   #Admin 
+
+@api.route('/sign_up_admin', methods=['POST']) 
+def sign_up_admin():
+
+    body_params = request.get_json()
+    print(body_params)
+    name = body_params.get("name", None)
+    last_name = body_params.get("last_name", None)
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+    tipo = body_params.get("tipo", None)
+    
+    user1 = Admin(name=name, last_name = last_name, email=email, password=password, tipo= tipo)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "El usuario fue creado exitosamente"}), 200
+
+
