1dc9a4ed6a83cbf724d2a0098c337890bc8839c9
psoto-go <pabletesglez@gmail.com>
2021-08-06 20:41:20 +0000

    models

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    bookings = db.relationship('Booking', backref='user', lazy=True)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            
+        }
+
+    def check_password(self, password_param):
+        return safe_str_cmp(self.password.encode('utf-8'), password_param.encode('utf-8'))
+
+class Administrador(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+    def check_password(self, password_param):
+        return safe_str_cmp(self.password.encode('utf-8'),password_param.encode('utf-8'))
 
 
 class Hotel(db.Model):
@@ -9,6 +44,13 @@ class Hotel(db.Model):
     description = db.Column(db.String(1000), unique=False, nullable=False)
     location = db.Column(db.String(100), unique=False, nullable=False)
     rooms = db.relationship('Room', backref='hotel', lazy=True)
+    reviews = db.relationship('Review', backref='hotel', lazy=True)
+    questions = db.relationship('Question', backref='hotel', lazy=True)
+    services = db.relationship('service', secondary=services, lazy='subquery',
+        backref=db.backref('hotels', lazy=True))
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'),
+        nullable=False)
+    bookings = db.relationship('Booking', backref='hotel', lazy=True)
 
 
     def serialize(self):
@@ -18,6 +60,9 @@ class Hotel(db.Model):
             "description": self.description,
             "location": self.location,
             "rooms": list(map(lambda x:x.serialize(), self.rooms)),
+            "reviews": list(map(lambda x:x.serialize(), self.reviews)),
+            "questions": list(map(lambda x:x.serialize(), self.questions)),
+            "services": list(map(lambda x:x.serialize(), self.services))
 
             # do not serialize the password, its a security breach
         }
@@ -29,6 +74,7 @@ class Room(db.Model):
     availability = db.Column(db.Date, unique=False, nullable=False)
     hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'),
         nullable=False)
+    bookings = db.relationship('Booking', backref='room', lazy=True)
     def serialize(self):
         return {
             "id": self.id,
@@ -40,7 +86,84 @@ class Room(db.Model):
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    kind = db.Column(db.String(120), unique=False, nullable=False)
-    number_of_beds = db.Column(db.Integer, unique=False, nullable=False)
-    availability = db.Column(db.Date, unique=False, nullable=False)
-    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'),
+        nullable=False)
+    characteristic = db.Column(db.String(200), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": list(map(lambda x:x.serialize(), self.user_id)),
+            "description": self.description,
+            "hotel_id": list(map(lambda x:x.serialize(), self.hotel_id)),
+            "characteristic": self.characteristic
+            # do not serialize the password, its a security breach
+        }
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), unique=False, nullable=False)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'),
+        nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "hotel_id": list(map(lambda x:x.serialize(), self.hotel_id))
+            # do not serialize the password, its a security breach
+        }
+
+services = db.Table('services',
+    db.Column('service_id', db.Integer, db.ForeignKey('service.id'), primary_key=True),
+    db.Column('hotel_id', db.Integer, db.ForeignKey('hotel.id'), primary_key=True)
+)
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+            # do not serialize the password, its a security breach
+        }
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    hotels = db.relationship('Hotel', backref='city', lazy=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hotels": list(map(lambda x:x.serialize(), self.hotels))
+            # do not serialize the password, its a security breach
+        }
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'),
+        nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'),
+        nullable=False)
+    
+    
+
+    def serialize(self):
+        return {
+            "id": self.id
+            
+            # do not serialize the password, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,79 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Administrador
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/sign_up', methods=['POST']) 
+def sign_up_user():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
+    body_params = request.get_json()
+    print(body_params)
+    name = body_params.get("name", None)
+    last_name = body_params.get("last_name", None)
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
 
-    return jsonify(response_body), 200
+    
+    user1 = User(name=name, last_name = last_name, email=email, password=password)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "El usuario fue creado exitosamente"}), 200
+
+
+@api.route('/sign_in', methods=['POST']) 
+def sign_in_user():
+
+    body_params = request.get_json()
+
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+
+    if email == None or password == None:
+        return jsonify({"msg" : "Error en el email o en la contraseña"}), 401
+    
+    user = User.query.filter_by(email=email).one_or_none()
+    admin = Administrador.query.filter_by(email=email).one_or_none()
+
+    if not user and not admin :
+        return jsonify("Your credentials are wrong, please try again"), 401
+
+
+    if user and user.check_password(password):
+        access_token = create_access_token(identity=user.serialize())
+        return jsonify({"access_token":  access_token, "user": user.serialize()}), 200
+    
+    elif admin and admin.check_password(password):
+        access_token = create_access_token(identity=admin.serialize())
+        return jsonify({"access_token":  access_token, "admin": admin.serialize()}), 200
+        
+
+    return jsonify({"hola"}), 200
+
+   #User
+ # --------------------------------------------------------------------------------------------
+   #Admin 
+
+@api.route('/sign_up_admin', methods=['POST']) 
+def sign_up_admin():
+
+    body_params = request.get_json()
+    print(body_params)
+    name = body_params.get("name", None)
+    last_name = body_params.get("last_name", None)
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+    
+    user1 = Administrador(name=name, last_name = last_name, email=email, password=password)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "El usuario fue creado exitosamente"}), 200
