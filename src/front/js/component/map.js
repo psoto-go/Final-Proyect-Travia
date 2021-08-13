c89dc7ddedc32c4d1e9927d1b0d7619b6cf58c2b
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-08-13 18:31:00 +0000

    map act

--- src/front/js/component/map.js
+++ src/front/js/component/map.js
@@ -5,12 +5,27 @@
 
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import Chinchetas from "./chinchetas.json";
+import PropTypes from "prop-types";
 
+const Pin = ({ text }) => (
+	<img
+		style={{
+			textAlign: "center",
+			transform: "translate(-50%, -50%)",
+			position: "absolute",
+			height: "3em",
+			width: "2em"
+		}}
+		src={"http://www.clker.com/cliparts/e/3/F/I/0/A/google-maps-marker-for-residencelamontagne-hi.png"}
+		alt={text}
+	/>
+);
 export const Map = () => {
 	const center = {
 		center: {
-			lat: 40.58674240500642,
-			lng: -82.2249773351358
+			lat: -34.5453018,
+			lng: -58.4519689
 		},
 		zoom: 12
 	};
@@ -23,8 +38,23 @@ export const Map = () => {
 				bootstrapURLKeys={{ key: "AIzaSyDaq_DXg5Oe1n6XQFUFMQu8EusN8NOXhZY" }} // api key hidden
 				defaultCenter={center.center}
 				defaultZoom={center.zoom}
-				yesIWantToUseGoogleMapApiInternals
-			/>
+				yesIWantToUseGoogleMapApiInternals>
+				{Chinchetas.features.map(chinche => {
+					console.log(chinche.geometry.coordinates[0]);
+					return (
+						<Pin
+							text="blabla"
+							key={chinche.properties.PARK_ID}
+							lat={chinche.geometry.coordinates[0]}
+							lng={chinche.geometry.coordinates[1]}
+						/>
+					);
+				})}
+			</GoogleMapReact>
 		</div>
 	);
+};
+
+Pin.propTypes = {
+	text: PropTypes.string
 };
