7fc9822d3c0a9e06a94961f0788d26663379e3bd
psoto-go <pabletesglez@gmail.com>
2021-08-20 19:27:08 +0000

    Main

--- src/front/js/component/destinations.js
+++ src/front/js/component/destinations.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Component, useContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { api_url } from "../constants";
 
 import { Featured } from "./featured";
 export const Destinations = () => {
+	const [detalles, setDetalles] = useState();
+
+	useEffect(() => {
+		fetch("https://3001-peach-bear-cn1jkr6r.ws-eu16.gitpod.io/api/featuredhotels")
+			.then(response => response.json())
+			.then(result => {
+				setDetalles(result.response);
+
+				console.log("hotellaaa");
+			})
+			.catch(error => console.log("Error", error));
+	}, []);
+	console.log(detalles);
 	return (
 		<>
 			<div className="lugares">
@@ -56,22 +72,35 @@ export const Destinations = () => {
 					</li>
 				</ul>
 			</div>
-			<div className="imgRedonda">
-				<img
-					src="https://images.squarespace-cdn.com/content/v1/5a86b05bcf81e0af04936cc7/1586529185422-B9XBTSLKN3KFZDYBGKFI/que-ver-en-asturias-picos-portada.jpg?format=1500w"
-					alt="..."
-				/>
-			</div>
-			<div>
-				<div className="card-body positionText">
-					<h5 className="card-title p-3 ml-5">Belmonte de Miranda</h5>
-					<p className="card-text ml-8">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply
-						dummy text of the printing and typesetting industry.
-					</p>
+			<div className="tab-content" id="myTabContent">
+				<div className="tab-pane fade show active" id="Asturias" role="tabpanel" aria-labelledby="asturias-tab">
+					<div className="imgRedonda">
+						<img
+							src="https://images.squarespace-cdn.com/content/v1/5a86b05bcf81e0af04936cc7/1586529185422-B9XBTSLKN3KFZDYBGKFI/que-ver-en-asturias-picos-portada.jpg?format=1500w"
+							alt="..."
+						/>
+					</div>
+					<div>
+						<div className="card-body positionText">
+							<h5 className="card-title p-3 ml-5">Belmonte de Miranda</h5>
+							<p className="card-text ml-8">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+								is simply dummy text of the printing and typesetting industry.
+							</p>
+						</div>
+					</div>
+					<Featured />
+				</div>
+				<div className="tab-pane fade" id="Murcia" role="tabpanel" aria-labelledby="murcia-tab">
+					...asdfasdfasdfa
+				</div>
+				<div className="tab-pane fade" id="Mallorca" role="tabpanel" aria-labelledby="mallorca-tab">
+					..aa.
+				</div>
+				<div className="tab-pane fade" id="Canarias" role="tabpanel" aria-labelledby="canarias-tab">
+					..aa.
 				</div>
 			</div>
-			<Featured />
 		</>
 	);
 };
