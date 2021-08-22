518b189f4b4dfbbf8dd35fd49e8ed18cd193485a
enaguero <enaguero@gmail.com>
2021-08-22 19:17:20 +0000

    Refactoring code

--- src/front/js/component/destinations.js
+++ src/front/js/component/destinations.js
@@ -5,7 +5,7 @@ import { api_url } from "../constants";
 import { Featured } from "./featured";
 
 export const Destinations = () => {
-	const [detalles, setDetalles] = useState();
+	const [detalles, setDetalles] = useState([]);
 
 	useEffect(() => {
 		fetch(api_url + "/api/cities")
@@ -15,66 +15,55 @@ export const Destinations = () => {
 			})
 			.catch(error => console.log("Error", error));
 	}, []);
+
+	const listCities = detalles.map((item, index) => {
+		return (
+			<li key={index} className="nav-item" role="presentation">
+				<a
+					className={index != 0 ? "nav-link" : "nav-link active"}
+					id={`pills-${item.id}-tab`}
+					data-toggle="pill"
+					href={`#pills-${item.id}`}
+					role="tab"
+					aria-controls={`pills-${item.id}`}
+					aria-selected={index != 0 ? "false" : "true"}>
+					{item.name}
+				</a>
+			</li>
+		);
+	});
+
+	const listCitiesDetails = detalles.map((item, index) => {
+		return (
+			<div
+				key={index}
+				className={index != 0 ? "tab-pane fade" : "tab-pane fade show active"}
+				id={`pills-${item.id}`}
+				role="tabpanel"
+				aria-labelledby={`pills-${item.id}-tab`}>
+				<div className="imgRedonda">
+					<img src={item.url} alt="..." />
+				</div>
+				<div>
+					<div className="card-body positionText">
+						<h5 className="card-title p-3 ml-5">{item.name}</h5>
+						<p className="card-text ml-8">{item.description}</p>
+					</div>
+				</div>
+			</div>
+		);
+	});
+
 	return (
-		<>
+		<div>
 			<div className="lugares">
 				<ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-					{detalles
-						? detalles.map((item, index) => {
-								return (
-									<li key={index} className="nav-item" role="presentation">
-										<a
-											className={index != 0 ? "nav-link" : "nav-link active"}
-											id={`pills-${detalles[index].id}-tab`}
-											data-toggle="pill"
-											href={`#pills-${detalles[index].id}`}
-											role="tab"
-											aria-controls={`pills-${detalles[index].id}`}
-											aria-selected={index != 0 ? "false" : "true"}>
-											{detalles[index].name}
-										</a>
-									</li>
-								);
-						  })
-						: "adsfa"}
+					{listCities}
 				</ul>
 			</div>
 			<div className="tab-content" id="pills-tabContent">
-				{detalles
-					? detalles.map((item, index) => {
-							return (
-								<div
-									key={index}
-									className={index != 0 ? "tab-pane fade" : "tab-pane fade show active"}
-									id={`pills-${detalles[index].id}`}
-									role="tabpanel"
-									aria-labelledby={`pills-${detalles[index].id}-tab`}>
-									<div className="imgRedonda">
-										<img src={detalles[index].url} alt="..." />
-									</div>
-									<div>
-										<div className="card-body positionText">
-											<h5 className="card-title p-3 ml-5">{detalles[index].name}</h5>
-											<p className="card-text ml-8">{detalles[index].description}</p>
-										</div>
-									</div>
-									{detalles[index].hotels
-										? detalles[index].hotels.map((item, indexa) => {
-												return (
-													<Featured
-														key={indexa}
-														name={detalles[index].hotels[indexa].name}
-														url={detalles[index].hotels[indexa].HotelArchives[0].url}
-														city_id={detalles[index].name}
-													/>
-												);
-										  })
-										: "adsfa"}
-								</div>
-							);
-					  })
-					: "adsfa"}
+				{listCitiesDetails}
 			</div>
-		</>
+		</div>
 	);
 };
