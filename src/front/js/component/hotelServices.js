43cb71364a9a6f14f778cfea5903801ffa32db0f
psoto-go <pabletesglez@gmail.com>
2021-08-24 11:38:18 +0000

    add

--- src/front/js/component/hotelServices.js
+++ src/front/js/component/hotelServices.js
@@ -1,25 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, useContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { api_url } from "../constants";
 
-export const HotelServices = () => {
+export const HotelServices = props => {
 	return (
-		<div className="serviciosHotel">
-			<h3>Servicios del Hotel</h3>
-
+		<>
 			<button type="button" className="btn btn-light bg-info text-white">
-				Piscina
+				{props.service}
 			</button>
-
-			<button type="button" className="btn btn-light bg-info text-white">
-				Restaurante
-			</button>
-
-			<button type="button" className="btn btn-light bg-info text-white">
-				Gimnasio
-			</button>
-			<br />
-			<a className="text-info" href="#">
-				ver todos los servicios
-			</a>
-		</div>
+		</>
 	);
+};
+
+HotelServices.propTypes = {
+	service: PropTypes.string
 };
