d4826854a83120c780d62f920b959455a73ad4f3
enaguero <enaguero@gmail.com>
2021-08-25 17:41:47 +0000

    Complete structure to search by params

--- src/api/hotel_searcher.py
+++ src/api/hotel_searcher.py
@@ -11,14 +11,36 @@ class HotelSearcher:
     self.end_date=end_date
 
   def search(self):
-    if self.city_id is not None:
+    
+    if self.city_id is not None and self.people is not None and self.start_date is not None and self.end_date is not None:
+      return self.all()
+    else if self.city_id is not None and self.people is not None and self.start_date is not None and self.end_date is None:
+      return self.by_city_capacity_start_date()
+    else if self.city_id is not None and self.people is not None and self.start_date is None and self.end_date is None:
+      return self.by_city_capacity()
+    else if self.city_id is not None and self.people is None and self.start_date is not None and self.end_date is not None:
+      return self.by_city_period()
+    else if self.city_id is not None and self.people is None and self.start_date is not None and self.end_date is None:
+      return self.by_city_start_date()
+    else if self.city_id is not None and self.people is None and self.start_date is None and self.end_date is None:
       return self.by_city()
     else:
       return self.all()
 
   def by_city(self):
-
     return Hotel.query.join(City).filter(City.id == self.city_id)
 
-  def all(self):
-    return Hotel.query.all()
+  def by_city_start_date(self):
+    pass
+
+  def by_city_period(self):
+    pass
+
+  def by_city_capacity(self):
+    pass
+
+  def by_city_capacity_start_date(self):
+    pass
+
+  def by_all(self)
+    pass
