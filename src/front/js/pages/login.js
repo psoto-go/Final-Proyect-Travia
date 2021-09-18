4d298911fcff409a5ae291f2ef05095695590927
soydeif <daed_diaz@hotmail.com>
2021-09-18 10:40:35 +0000

    login modal in progress not finished

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -8,6 +8,7 @@ import google from "../../img/googlelogo.png";
 import apple from "../../img/apple.png";
 import GoogleLogin from "react-google-login";
 import jwt from "jwt-decode"; // import dependency
+import "/workspace/Final-Proyect-Travia/src/front/styles/modalForm.scss";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
@@ -55,8 +56,9 @@ export const Login = () => {
 			actions.signin_google(respuesta);
 		}
 	};
+
 	return (
-		<div className="offset-4 col-4 my-auto text-center">
+		/*<div className="offset-4 col-4 my-auto text-center">
 			<form className="form-signin" onSubmit={submitForm}>
 				<img className="mb-4" src={logo} alt="" width="72" height="72" />
 				<h1 className="text-center mb-3 font-weight-normal">Inicia sesión</h1>
@@ -118,6 +120,71 @@ export const Login = () => {
 				<h2 />
 				<hr />
 			</form>
+		</div>*/
+		<div className="bg-modal">
+			<div className="modal-content">
+				<form className="form-signin" onSubmit={submitForm}>
+					<img src={logo} alt="" width="72" height="72" />
+					<h1 className="text-center mb-3 font-weight-normal">Inicia sesión</h1>
+					<hr />
+					<div className="form-group row">
+						<label htmlFor="inputEmail" className="col-sm-2 col-form-label">
+							Email
+						</label>
+
+						<div className="col-sm-10">
+							<input
+								type="email"
+								className="form-control"
+								id="exampleFormControlInput1"
+								placeholder="Email"
+								name="email"
+								onChange={changeInput}
+							/>
+						</div>
+					</div>
+					<div className="form-group row">
+						<label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+							Contraseña
+						</label>
+						<div className="col-sm-10">
+							<input
+								type="password"
+								className="form-control"
+								id="inputPassword"
+								placeholder="Password"
+								name="password"
+								onChange={changeInput}
+							/>
+						</div>
+					</div>
+					<div className="checkbox mb-3">¿Olvidaste tu contraseña?</div>
+					<button className="btn btn-lg btn-primary btn-block" type="submit">
+						Iniciar sesión
+					</button>
+					<p className="text-right p-2">¿No tienes cuenta? Regístrate</p>
+					<hr className="hr-text" data-content="o usar una de estas opciones" />
+					<div className="d-flex justify-content-center">
+						<img className="mr-5" src={facebook} alt="" width="72" height="72" />
+
+						<GoogleLogin
+							clientId="554543668987-m44m7icesa0r453l0md9969mkv5me163.apps.googleusercontent.com"
+							onSuccess={respuestaGoogle}
+							onFailure={respuestaGoogle}
+							cookiePolicy={"single_host_origin"}
+						/>
+
+						<img className="mr-5" src={apple} alt="" width="52" height="64" />
+					</div>
+
+					<p className="text-center mt-5 mb-3 text-muted">
+						Al iniciar sesión o al crear una cuenta, aceptas nuestros Términos y condiciones y la Política
+						de privacidad
+					</p>
+					<h2 />
+					<hr />
+				</form>
+			</div>
 		</div>
 	);
 };
