fa0450a6729af7ec2a7e7df606b49b1dbdfb0e18
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-09-22 01:02:05 +0000

    responsive image and h1 header

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import "../../styles/homeBackground/homeBackground.scss";
-import logo from "../../img/golondrinablanco.png";
+import header2 from "../../img/header2.png";
 import header from "../../img/header.png";
-import { Search } from "./search";
+
 export const Header = () => {
 	return (
 		<div className="header">
-			<div className="contenedor">
+			<div className="contenedor ">
 				<h1 className="coloredh1">Travia.</h1>
-				<h3>Es distinto, es Travia.</h3>
+				<h3 className="coloredh3">Es distinto, es Travia.</h3>
 			</div>
 			<div id="landingImg">
 				<img src={header} />
+			</div>
+			<div id="landingImg2">
+				<img src={header2} />
 			</div>
 		</div>
 	);
