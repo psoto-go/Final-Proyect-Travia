aa4b0046eb77c6b339ad2f53e210b8db3d57ed14
psoto-go <pabletesglez@gmail.com>
2021-09-24 18:48:39 +0000

    seed photo

--- migrations/versions/69f7a316e390_.py
+++ migrations/versions/69f7a316e390_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f054390455ca
+Revision ID: 69f7a316e390
 Revises: 
-Create Date: 2021-09-24 17:08:58.583937
+Create Date: 2021-09-24 18:47:32.533602
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f054390455ca'
+revision = '69f7a316e390'
 down_revision = None
 branch_labels = None
 depends_on = None
