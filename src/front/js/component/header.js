1a2fbf56ac55d3e54740b0cce5812bb3c0e2db83
soydeif <daed_diaz@hotmail.com>
2021-09-25 17:44:53 +0000

    fixing responsive

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -9,7 +9,7 @@ export const Header = () => {
 		<div className="header">
 			<div className="ml-3 mr-3 justify-content-center">
 				<h1 className="coloredh1 ml-5 pt-5">Lorem ipsum dolor sit amet</h1>
-				<h3 className=" ml-5">Lorem ipsum dolor </h3>
+				<h3 className=" ml-5 d-none d-lg-block d-xl-block">Lorem ipsum dolor </h3>
 			</div>
 			<div id="landingImg">
 				<img src={header} className="d-none d-lg-block d-xl-block" />
