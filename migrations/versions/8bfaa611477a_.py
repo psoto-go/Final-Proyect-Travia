74820ca4b2a93a5a03ffc07c2948d4c0b9c60438
soydeif <daed_diaz@hotmail.com>
2021-09-25 16:08:11 +0000

    .

--- migrations/versions/8bfaa611477a_.py
+++ migrations/versions/8bfaa611477a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b6369818a4e8
+Revision ID: 8bfaa611477a
 Revises: 
-Create Date: 2021-09-22 18:51:58.058066
+Create Date: 2021-09-25 16:05:41.495982
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b6369818a4e8'
+revision = '8bfaa611477a'
 down_revision = None
 branch_labels = None
 depends_on = None
