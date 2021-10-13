40f5f7918aeb3f3de7d1e6e30f6f2cf0c8d40c4e
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-10-13 22:43:04 +0000

    fixed hotelDetail col

--- src/front/js/pages/hotelDetail.js
+++ src/front/js/pages/hotelDetail.js
@@ -7,7 +7,7 @@ import { Availability } from "../component/availability";
 import { HotelPrices } from "../component/hotelPrices";
 import { Reviews } from "../component/reviews";
 import { HotelServices } from "../component/hotelServices";
-import { PruebaCarousel } from "../component/PruebaCarousel/PruebaCarousel";
+import { PruebaCarousel2 } from "../component/PruebaCarousel/PruebaCarousel2";
 
 export const HotelDetail = () => {
 	const [hotel, setHotel] = useState({});
@@ -64,8 +64,8 @@ export const HotelDetail = () => {
 
 	return (
 		<div className="row">
-			<div className="m-5 col-lg-6">
-				<h2 className="m-3 ">{hotel.name}</h2>
+			<div className="col-12">
+				{/* <h2 className="m-3 ">{hotel.name}</h2> */}
 				{/* {listGallery} */}
 				<div>
 					<PruebaCarousel2 />
