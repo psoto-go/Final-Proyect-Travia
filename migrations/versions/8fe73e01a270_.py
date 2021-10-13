cfa68db59cf10752294c688f5e88f9a4ac805269
soydeif <daed_diaz@hotmail.com>
2021-10-13 02:54:02 +0000

    resposive searchbar list

--- migrations/versions/8fe73e01a270_.py
+++ migrations/versions/8fe73e01a270_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 22b2fcb24fd7
+Revision ID: 8fe73e01a270
 Revises: 
-Create Date: 2021-10-11 09:08:37.969139
+Create Date: 2021-10-12 22:59:20.926134
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '22b2fcb24fd7'
+revision = '8fe73e01a270'
 down_revision = None
 branch_labels = None
 depends_on = None
